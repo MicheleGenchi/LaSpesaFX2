5e6289168eda32082c151eb9a2f2b76f685bf470
JAVASE <JAVASE@JAVASE-PC.localdomain>
2018-03-25 12:18:47 +0200

    InjectionMainController nella classe MainController

--- src/application/control/MainController.java
+++ src/application/control/MainController.java
@@ -1,5 +1,54 @@
 package application.control;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+
 public class MainController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private MenuItem mnuLogin;
+
+    @FXML
+    private MenuItem mnuChiudi;
+
+    @FXML
+    private MenuItem mnuInformazioni;
+
+    @FXML
+    void doMnuChiudi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doMnuInformazioni(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doMnuLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert borderPane != null : "fx:id=\"borderPane\" was not injected: check your FXML file 'Main.fxml'.";
+        assert mnuLogin != null : "fx:id=\"mnuLogin\" was not injected: check your FXML file 'Main.fxml'.";
+        assert mnuChiudi != null : "fx:id=\"mnuChiudi\" was not injected: check your FXML file 'Main.fxml'.";
+        assert mnuInformazioni != null : "fx:id=\"mnuInformazioni\" was not injected: check your FXML file 'Main.fxml'.";
+        TableController.injectionMainController(this);
+    }
 }
+
